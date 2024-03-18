e5f075bea817af7e3c3aa6b7f14bca2bb1cace11
JNicolao <bororio96@gmail.com>
2024-03-18 23:12:02 +0300

    final user interface updates

--- src/app/components/ArticleItem.tsx
+++ src/app/components/ArticleItem.tsx
@@ -1,14 +1,13 @@
 interface ArticleItemProps {
-    title: string;
-    content: string;
-  }
-  
-  const ArticleItem: React.FC<ArticleItemProps> = ({ title, content }) => (
-    <article>
-      <h3 className="font-bold">{title}</h3>
-      <p className="font-light">{content}</p>
-    </article>
-  );
-  
-  export default ArticleItem;
-  
+  title: string;
+  content: string;
+}
+
+const ArticleItem: React.FC<ArticleItemProps> = ({ title, content }) => (
+  <article className="py-4">
+    <h3 className="font-bold mb-2">{title}</h3>
+    <p className="font-light line-clamp-2 my-1 text-gray-300  ">{content}</p>
+  </article>
+);
+
+export default ArticleItem;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import NavItem from "./components/NavItem";
 import ArticleItem from "./components/ArticleItem";
 import ImageComponent from "./components/ImageComponent";
@@ -8,31 +8,31 @@ import Image from 'next/image';
 
 const navItems = ["Home", "News", "Popular", "Trending", "Categories"];
 const articles = [
-  { title: "Decentralized Finance (DeFi)", content: "Transforming traditional finance by leveraging blockchain technology to facilitate open, accessible, and secure financial markets." },
-  { title: "Non-Fungible Tokens (NFTs)", content: "Revolutionizing digital ownership and creativity through unique, blockchain-based digital assets that represent art, collectibles, and more." },
-  { title: "Decentralized Autonomous Organizations (DAOs)", content: "Enabling a new form of organizational structure governed by smart contracts, allowing for community-driven decisions and operations." },
+    { title: "Decentralized Finance (DeFi)", content: "Transforming traditional finance by leveraging blockchain technology to facilitate open, accessible, and secure financial markets." },
+    { title: "Non-Fungible Tokens (NFTs)", content: "Revolutionizing digital ownership and creativity through unique, blockchain-based digital assets that represent art, collectibles, and more." },
+    { title: "Decentralized Autonomous Organizations (DAOs)", content: "Enabling a new form of organizational structure governed by smart contracts, allowing for community-driven decisions and operations." },
 ];
 const listItems = [
     {
-      src: "/images/image-retro-pcs.jpg",
-      alt: "DeFi",
-      title: "01",
-      heading: "Exploring DeFi",
-      text: "Discover how decentralized finance is creating new opportunities for financial ",
+        src: "/images/image-retro-pcs.jpg",
+        alt: "DeFi",
+        title: "01",
+        heading: "Exploring DeFi",
+        text: "Discover how decentralized finance is creating new opportunities for financial ",
     },
     {
-      src: "/images/image-top-laptops.jpg",
-      alt: "NFTs",
-      title: "02",
-      heading: "NFTs and Digital Ownership",
-      text: "Understand the impact of Non-Fungible Tokens on digital art, gaming, and beyond",
+        src: "/images/image-top-laptops.jpg",
+        alt: "NFTs",
+        title: "02",
+        heading: "NFTs and Digital Ownership",
+        text: "Understand the impact of Non-Fungible Tokens on digital art, gaming, and beyond",
     },
     {
-      src: "/images/image-gaming-growth.jpg",
-      alt: "DAOs",
-      title: "03",
-      heading: "The Rise of DAOs",
-      text: "Learn about Decentralized Autonomous Organizations and how they're redefining ",
+        src: "/images/image-gaming-growth.jpg",
+        alt: "DAOs",
+        title: "03",
+        heading: "The Rise of DAOs",
+        text: "Learn about Decentralized Autonomous Organizations and how they're redefining ",
     },
 ];
 
@@ -41,35 +41,38 @@ export default function Home() {
 
     return (
         <main className="flex flex-col items-center justify-between md:p-16 p-6 max-w-[1200px] m-auto min-h-screen">
-            
-            <header className="flex w-full items-end justify-between">
+
+            <header className="flex w-full justify-between">
                 <div>
-                <ImageComponent src="/images/logo.svg" alt="" className="bg-white" width={40} height={40}/>
+                    <ImageComponent src="/images/logo.svg" alt="" className="bg-white" width={40} height={40} />
                 </div>
-                <div className='md:hidden' onClick={() => setIsNavOpen(!isNavOpen)}>
-                    <ImageComponent src={isNavOpen ? "/images/icon-menu-close.svg" : "/images/icon-menu.svg"} alt="Menu Icon" className="bg-white absolute top-6 right-6 " width={30} height={30}/>
-                </div>
-                <div className='absolute md:static top-[55px] right-6 z-10 bg-black w-[200px] md:w-[35%] md:mr-0'>
-                <NavMenu isNavOpen={isNavOpen} navItems={navItems} />
+                <div>
+                    <div className='md:hidden' onClick={() => setIsNavOpen(!isNavOpen)}>
+                        <ImageComponent src={isNavOpen ? "/images/icon-menu-close.svg" : "/images/icon-menu.svg"} alt="Menu Icon" className="bg-white absolute top-6 right-6 " width={30} height={30} />
+                    </div>
+                    <div className='absolute md:static z-10'>
+                        <NavMenu isNavOpen={isNavOpen} navItems={navItems} />
+                    </div>
                 </div>
             </header>
-                
+
             <section className='flex flex-col md:flex-row justify-between py-10 gap-6 items-stretch'>
-                <div className='flex flex-col md:flex-2 md:gap-8'>
-                    <ImageComponent src="/images/image-web-3-desktop.jpg" alt="Web 3.0 Overview" width={650} height={150}/>
+                <div className='flex flex-col gap-4 md:flex-2 md:gap-8'>
+                    <ImageComponent src="/images/image-web-3-desktop.jpg" alt="Web 3.0 Overview" width={650} height={150} />
                     <div className='flex gap-4 flex-col md:flex-row'>
-                        <h2 className='flex-1 text-5xl font-extrabold'>The Bright Future of Web 3.0</h2>
-                        <div className='md:flex-1'>
-                            <p className='flex-1'>Web 3.0 represents the next phase of the internet's evolution, offering decentralized and transparent user experiences powered by blockchain technology. </p>
-                            <button className='p-2 bg-red-500 my-2 w-[50%] uppercase align-middle '>Read More</button>
+                        <h2 className='flex-1 text-5xl font-extrabold'>The Bright Future of Web 3.0?</h2>
+                        <div className='flex flex-col gap-4 md:flex-1' >
+                            <p className='flex-1 text-gray-500 text-sm'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+                                But is it really fulfilling its promise?</p>
+                            <button className='p-2 bg-red-500 my-2 w-[50%] uppercase align-middle text-white tracking-wider'>Read More</button>
                         </div>
                     </div>
                 </div>
-                <aside className='md:flex-2 bg-black'>
-                    <h2 className='text-3xl text-amber-500 font-semibold'>What's New</h2>
+                <aside className='md:flex-2 bg-slate-950 text-white  p-6'>
+                    <h2 className='text-3xl text-amber-300 font-bold'>New</h2>
                     <div className=''>
                         {articles.map((article, index) => (
-                            <div className='border border-transparent border-b-white py-3 text-md' key={index}>
+                            <div className='border border-transparent border-b-white last:border-b-0 text-md' key={index}>
                                 <ArticleItem title={article.title} content={article.content} />
                             </div>
                         ))}
@@ -80,12 +83,12 @@ export default function Home() {
             <section>
                 <ul className='flex flex-col md:flex-row items-center content-center justify-evenly gap-8'>
                     {listItems.map((item, index) => (
-                        <li key={index} className='flex gap-2 items-center content-between'>
+                        <li key={index} className='flex gap-4 items-center content-between'>
                             <ImageComponent src={item.src} alt={item.alt} width={100} height={100} />
                             <div>
                                 <h3 className='text-2xl font-semibold text-gray-400'>{item.title}</h3>
-                                <h3 className="text-lg font-bold ">{item.heading}</h3>
-                                <p className='text-sm md:text-md'>{item.text}</p>
+                                <h3 className="text-lg font-bold py-2 ">{item.heading}</h3>
+                                <p className='text-sm md:text-md line-clamp-2 text-gray-500'>{item.text}</p>
                             </div>
                         </li>
                     ))}
