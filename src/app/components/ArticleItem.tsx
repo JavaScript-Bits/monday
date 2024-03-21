0394bf6343778aa5ebac64cf2e071a5c4d68a09f
JNicolao <bororio96@gmail.com>
2024-03-21 19:15:27 +0300

    fixed mobile menu navbar

--- src/app/components/ArticleItem.tsx
+++ src/app/components/ArticleItem.tsx
@@ -5,8 +5,8 @@ interface ArticleItemProps {
 
 const ArticleItem: React.FC<ArticleItemProps> = ({ title, content }) => (
   <article className="py-4">
-    <h3 className="font-bold mb-2">{title}</h3>
-    <p className="font-light line-clamp-2 my-1 text-gray-300  ">{content}</p>
+    <h3 className="font-extrabold text-lg mt-2">{title}</h3>
+    <p className="text-lg line-clamp-2 text-gray-300 mt-2">{content}</p>
   </article>
 );
 
